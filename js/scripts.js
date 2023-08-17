7023f3794761cc9a652766761bb0721a70a8d4d0
Taylor <taylordavids997@gmail.com>
2023-08-17 13:19:39 +0200

    add values

--- js/scripts.js
+++ js/scripts.js
@@ -13,25 +13,25 @@ const order1_status = document.querySelector('[data-key="order1"] .status ')
 order1_status.innerHTML = 'Pending'
 // Second Order
 const order2_biscuits = document.querySelector('[data-key="order2"] .biscuits .count')
-order2_biscuits.innerHTML = '10'
+order2_biscuits.innerHTML = '5'
 
 const order2_donuts = document.querySelector('[data-key="order2"] .donuts .count')
-order2_donuts.innerHTML = '13'
+order2_donuts.innerHTML = '0'
 
 const order2_pancakes = document.querySelector('[data-key="order2"] .pancakes .count')
-order2_pancakes.innerHTML = '0'
+order2_pancakes.innerHTML = '2'
 
 const order2_status = document.querySelector('[data-key="order2"] .status ')
 order2_status.innerHTML = 'Pending'
 // Third Order
 const order3_biscuits = document.querySelector('[data-key="order3"] .biscuits .count')
-order3_biscuits.innerHTML = '10'
+order3_biscuits.innerHTML = '12'
 
 const order3_donuts = document.querySelector('[data-key="order3"] .donuts .count')
-order3_donuts.innerHTML = '13'
+order3_donuts.innerHTML = '11'
 
 const order3_pancakes = document.querySelector('[data-key="order3"] .pancakes .count')
-order3_pancakes.innerHTML = '0'
+order3_pancakes.innerHTML = '15'
 
 const order3_status = document.querySelector('[data-key="order3"] .status ')
 order3_status.innerHTML = 'Delivered'
